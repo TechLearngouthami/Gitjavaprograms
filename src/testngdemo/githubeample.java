c2b94bfe7a26c674e295a75b3b5115dfc88eeddf
TechLearngouthami <163445767+TechLearngouthami@users.noreply.github.com>
2024-03-15 22:51:56 +0530

    githubexa

---
+++ src/testngdemo/githubeample.java
@@ -0,0 +1,14 @@
+package testngdemo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class githubeample {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
