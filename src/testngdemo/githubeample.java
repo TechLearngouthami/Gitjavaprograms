a1b61cd3496d0c804c941b4a3fc8cf927cda2002
TechLearngouthami <163445767+TechLearngouthami@users.noreply.github.com>
2024-03-15 23:06:00 +0530

    code modified

--- src/testngdemo/githubeample.java
+++ src/testngdemo/githubeample.java
@@ -1,14 +1,21 @@
 package testngdemo;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class githubeample {
+	WebDriver driver;
+	
   @Test
   public void f() {
+	  driver.get("https://www.google.com/");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }
